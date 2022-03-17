2cc841dd8ab34082989a9132543c0042b9c8fbb1
Eilidh Morrice-Ashdown <eilidhma@gmail.com>
2022-03-16 20:38:46 -0700

    mail

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,25 +1,25 @@
-const mail = require('@sendgrid/mail');
+// const mail = require('@sendgrid/mail');
 
-mail.setApiKey(process.env.SENDGRID_API_KEY);
+// mail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default async(req, res) => {
-  const body = JSON.parse(req.body);
+// export default async(req, res) => {
+//   const body = JSON.parse(req.body);
 
-  const message = `
-    Name: ${body.name}\r\n
-    Email: ${body.email}\r\n
-    Message: ${body.message}
-  `;
+//   const message = `
+//     Name: ${body.name}\r\n
+//     Email: ${body.email}\r\n
+//     Message: ${body.message}
+//   `;
 
-  const data = {
-    to: 'eilidhma@gmail.com',
-    from: 'hello@eilidhmorriceashdown.com',
-    subject: 'New web form message!',
-    text: message,
-    html: message.replace(/\r\n/g, '<br>')
-  }
+//   const data = {
+//     to: 'eilidhma@gmail.com',
+//     from: 'hello@eilidhmorriceashdown.com',
+//     subject: 'New web form message!',
+//     text: message,
+//     html: message.replace(/\r\n/g, '<br>')
+//   }
 
-  mail.send(data)
+//   mail.send(data)
 
-  res.status(200).json({ status: 'Ok' });
-}
+//   res.status(200).json({ status: 'Ok' });
+// }
